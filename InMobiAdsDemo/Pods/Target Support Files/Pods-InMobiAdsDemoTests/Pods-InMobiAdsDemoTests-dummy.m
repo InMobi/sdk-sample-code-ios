0bb3a4072dc8739e1c16f4b100d846deb768abbc
Utpal Jha <utpal.jha@inmobi.com>
2017-10-04 15:18:49 +0530

    Pod folder removed

--- InMobiAdsDemo/Pods/Target Support Files/Pods-InMobiAdsDemoTests/Pods-InMobiAdsDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InMobiAdsDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_InMobiAdsDemoTests
-@end
--- InMobiAdsDemo/Pods/Target Support Files/Pods-InMobiAdsDemoUITests/Pods-InMobiAdsDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InMobiAdsDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_InMobiAdsDemoUITests
-@end
